07cae07d84b6632d49351280da1ac97883af6759
chubyx <carlosduquebravo@gmail.com>
2025-09-06 17:52:38 -0400

    Inicio de la aplicacion para idiomas

---
+++ app/src/main/java/com/example/lingogo/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.lingogo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button btnLogin = findViewById(R.id.btnLogin);
+
+        //Acción para presionar el boton
+        btnLogin.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, ConfigActivity.class);
+            startActivity(intent);
+        });
+
+    }
+}
